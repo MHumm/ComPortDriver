20f3b953bf37beb61f7459ed3dfbd7761b0e8c32
Markus Humm <markus.humm@gmail.com>
2025-07-23 22:30:12 +0200

    Added use of the new failure event all over
    Applied the mechanism of the new failure event to all places where Win API functions are being called. Usually the methods are quit at that point anyway or it is the last call in that method.

--- Source/ErrorCodes.pas
+++ Source/ErrorCodes.pas
@@ -6,15 +6,33 @@ const
   /// <summary>
   ///   More data to send given than could be send in a resonable time
   /// </summary>
-  cErrSendBufferFull = 1;
+  cErrSendBufferFull    = 1;
   /// <summary>
   ///   More data received than could stored in the receive buffer
   /// </summary>
   cErrReceiveBufferFull = 2;
+  /// <summary>
+  ///   Failure during listing the COM ports found on the system
+  /// </summary>
+  cErrEnumComPorts      = 3;
 
 resourcestring
-  rErrSendBufferFull    = 'Too much data to send specified';
-  rErrReceiveBufferFull = 'Too much data received';
+  rErrSendBufferFull      = 'Too much data to send specified';
+  rErrReceiveBufferFull   = 'Too much data received';
+  rErrSetCommBreakError   = 'Error in SetCommBreak. Reason: %0:s';
+  rErrApplyComSetsError   = 'Error in ApplyCOMSettings. '+ sLineBreak +
+                            'Reason: %0:s.' + sLineBreak +
+                            'Position: %1:s';
+  rErrClearCommBreakError = 'Error in ClearCommBreak. Reason: %0:s';
+  rErrConnectError        = 'Error connecting. Reason: %0:s';
+  rErrDisconnectError     = 'Error disconnecting. Reason: %0:s';
+  rErrReadError           = 'Error reading. Reason: %0:s';
+  rErrSendError           = 'Error sending. Reason: %0:s';
+  rErrGetModemStatusError = 'Error retrieving modem status. Reason: %0:s';
+  rErrFlushBuffersError   = 'Error flushing buffers. Reason: %0:s';
+  rErrClearCommError1     = 'Error in ClearCommError/CountRX. Reason: %0:s';
+  rErrClearCommError2     = 'Error in ClearCommError/OutFreeSpace. Reason: %0:s';
+  rErrEnumComPortsError   = 'Error during enumerating the comports of this system. Reason: %0:s';
 
 implementation
 
